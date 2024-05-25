ecfb92a950ca71e2c5cf2f83a1d5a8438e84f975
Beatriz Manrique <105998272+smavend@users.noreply.github.com>
2024-05-24 21:08:58 -0500

    Update A10_1.sh

--- Headnode/A10_1.sh
+++ Headnode/A10_1.sh
@@ -2,7 +2,7 @@
 
 wget http://download.cirros-cloud.net/0.4.0/cirros-0.4.0-x86_64-disk.img
 
-glance image-create --os-username '$OS_USERNAME' --name "cirros" --file ~/cirros-0.4.0-x86_64-disk.img --disk-format qcow2 --container-format bare --visibility=public
+glance image-create --name "cirros" --file ~/cirros-0.4.0-x86_64-disk.img --disk-format qcow2 --container-format bare --visibility=public
 
 # PREGUNTA 4: IMAGE LIST
 echo PREGUNTA4
